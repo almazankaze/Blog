8281b4a747032a53000dee4637e71992b8da9f4a
John Almazan <almazankaze@live.com>
2022-07-14 22:03:43 -0500

    added start graphqlQuery

--- my-blog/pages/index.js
+++ my-blog/pages/index.js
@@ -1,13 +1,8 @@
-import type { NextPage } from "next";
 import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "../components";
+import { getPosts } from "../services";
 
-const posts = [
-  { title: "React Testing", excerpt: "Learn React Testing" },
-  { title: "React with Tailwind", excerpt: "Learn React with Tailwind" },
-];
-
-const Home: NextPage = () => {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -18,7 +13,7 @@ const Home: NextPage = () => {
       <div className="grid grid-cols-1 lg:grid-cols-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post.node} key={post.title} />
           ))}
         </div>
 
@@ -31,6 +26,10 @@ const Home: NextPage = () => {
       </div>
     </div>
   );
-};
+}
 
-export default Home;
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return { props: { posts } };
+}
